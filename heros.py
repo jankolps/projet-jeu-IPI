8e79e6a71aacd73da19af568fc4cf3b7a2b9cdef
Jean Lepers <j2lepers@enib.fr>
2023-05-15 16:28:25 +0200

    Je commence le main.py

--- heros.py
+++ heros.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8  -*-
+# -*-    heros.py    -*-
+# -*-   J. Lepers    -*-
+# -*-       IPI      -*-
+
 import sys
 import time
 
---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8  -*-
+# -*-     main.py    -*-
+# -*-   J. Lepers    -*-
+# -*-       IPI      -*-
+
+import sys
+import time
+import termios
+import tty
+import select
+# import de librair
+
+import heros
+import background
+
+def init():
+
